06cd886f014f2e0363e3d386e03ec5befac9f3c9
shubhaamgupta11 <sg43245@gmail.com>
2025-02-10 17:11:19 +0530

    feat: added ga token

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from '@astrojs/partytown'
 
 import icon from "astro-icon";
 
@@ -10,8 +11,13 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   site: 'https://shubham-space.netlify.app',
   output: 'static',
-  integrations: [react(), icon()],
-
+  integrations: [
+    react(),
+    icon(),        
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      }})],
   vite: {
     plugins: [tailwindcss()],
   },
